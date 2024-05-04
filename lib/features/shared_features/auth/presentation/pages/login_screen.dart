2e57b54ba8fbd6a0f18898b81be048312731dce7
johny <104100765+johnykoudsy@users.noreply.github.com>
2024-05-04 12:43:05 +0300

    login

--- lib/features/shared_features/auth/presentation/pages/login_screen.dart
+++ lib/features/shared_features/auth/presentation/pages/login_screen.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:smart_recruitment_core/features/auth/presentation/bloc/user/user_bloc.dart';
 import 'package:smart_recruitment_core/utility/global_widgets/custom_text_field.dart';
 import 'package:smart_recruitment_core/utility/global_widgets/elevated_button_widget.dart';
 import 'package:smart_recruitment_core/utility/theme/color_style.dart';
 import 'package:smart_recruitment_core/utility/theme/text_styles.dart';
 import 'package:smart_recruitment_flutter_user/core/router/app_routes.dart';
+
+import '../../../../../utility/global_widgets/dialog_snack_bar.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -25,7 +29,22 @@ class _LoginScreenState extends State<LoginScreen> {
     double screenHeight = MediaQuery.of(context).size.height;
     final double heightBetweenFields = screenHeight * 0.015;
 
-    return Scaffold(
+    return BlocListener<UserBloc, UserState>(
+      listener: (context, state) {
+        if (state is UserLoggedState) {
+
+          Navigator.of(context).pushNamedAndRemoveUntil(
+              state.user.data.isCompany?
+              AppRoutes.recruiterBottomNavBar:AppRoutes.bottomNavBarScreen, (Route<dynamic> route) => false);
+        }
+        if (state is UserErrorState) {
+          DialogsWidgetsSnackBar.showSnackBarFromStatus(
+              context: context,
+              helperResponse: state.helperResponse,
+              showServerError: true);
+        }
+      },
+  child: Scaffold(
       body: Form(
         key: _key,
         child: ListView(
@@ -35,7 +54,9 @@ class _LoginScreenState extends State<LoginScreen> {
             Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                SizedBox(height: screenHeight*0.1,),
+                SizedBox(
+                  height: screenHeight * 0.1,
+                ),
                 ShaderMask(
                   blendMode: BlendMode.srcIn,
                   shaderCallback: (Rect bounds) {
@@ -72,7 +93,9 @@ class _LoginScreenState extends State<LoginScreen> {
                       hintText: '+96347924893',
                       textInputType: TextInputType.phone,
                     ),
-                    SizedBox(height: heightBetweenFields,),
+                    SizedBox(
+                      height: heightBetweenFields,
+                    ),
                     CustomTextField(
                       action: TextInputAction.done,
                       controller: passwordController,
@@ -98,22 +121,52 @@ class _LoginScreenState extends State<LoginScreen> {
                     ),
                   ],
                 ),
-                SizedBox(height: screenHeight*0.1,),
-                ElevatedButtonWidget(title: "Login",onPressed: (){},),
-                SizedBox(height: screenHeight*0.1,),
+                SizedBox(
+                  height: screenHeight * 0.1,
+                ),
+                BlocBuilder<UserBloc, UserState>(
+                  builder: (context, state) {
+                    return ElevatedButtonWidget(
+                      title: "Login",
+                      isLoading: state is UserLoading,
+                      onPressed: () {
+                        if (_key.currentState!.validate()) {
+                          context.read<UserBloc>().add(LoginUserEvent(
+                            password: passwordController.text,
+                            phoneNumber: phoneController.text,
+                          ));
+                        }
+                      },
+                    );
+                  },
+                ),
+                SizedBox(
+                  height: screenHeight * 0.1,
+                ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                  const Text("Don't have account?",style: AppFontStyles.mediumH4,),
-                  GestureDetector(
-                      onTap: (){Navigator.popAndPushNamed(context, AppRoutes.signup);},
-                      child: Text(" Signup",style: AppFontStyles.mediumH4.copyWith(color: AppColors.kMainColor100),))
-                ],)
+                    const Text(
+                      "Don't have account?",
+                      style: AppFontStyles.mediumH5,
+                    ),
+                    GestureDetector(
+                        onTap: () {
+                          Navigator.popAndPushNamed(context, AppRoutes.signup);
+                        },
+                        child: Text(
+                          " Signup",
+                          style: AppFontStyles.mediumH5
+                              .copyWith(color: AppColors.kMainColor100),
+                        ))
+                  ],
+                )
               ],
             ),
           ],
         ),
       ),
-    );
+    ),
+);
   }
 }
